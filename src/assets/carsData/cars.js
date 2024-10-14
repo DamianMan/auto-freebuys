316ba843ff2ea8504fb2bbc708e1d8140e9a4f54
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-10-14 18:39:37 +0200

    Updated cars

--- src/assets/carsData/cars.js
+++ src/assets/carsData/cars.js
@@ -87,7 +87,7 @@ const cars = [
     drivetrain: "Anteriore",
     features: ["Full Optional"],
     image:
-      "https://images.unsplash.com/photo-1630394781361-cce39e409555?q=80&w=1536&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.unsplash.com/photo-1711711112554-cc3c2da7d30e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym13JTIweDJ8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: "6",
